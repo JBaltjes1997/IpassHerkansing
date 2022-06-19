0f0f779ab0e978c852ab0b1f07327654a7fcaf89
Lobohuargo822 <jeroenbaltjes@hotmail.nl>
2022-06-19 22:15:23 +0200

    fix: createNewUserProfile refined

--- src/main/java/com/example/demo/data/databaseInfo.java
+++ src/main/java/com/example/demo/data/databaseInfo.java
@@ -8,19 +8,15 @@ import java.sql.Statement;
 
 public class databaseInfo {
 
-    public static Response createNewUserProfile(int id, String voornaam, String achternaam, String adres,
-                                                String postcode, String woonplaats, Date geboortedatum,
-                                                int telefoonnummer, String mailadres, String wachtwoord )
+    public static Response createNewUserProfile(int id, String voornaam, String achternaam, String wachtwoord )
                                                 throws SQLException {
 
         Connection connection = databaseQuery.getDBConnection();
         Statement statement = connection.createStatement();
 
         try {
-            statement.execute("INSERT INTO gebruiker(id, voornaam, achternaam, adres, postcode, woonplaats, \n" +
-                    "                    geboortedatum, telefoonnummer, mailadres, wachtwoord ) values ('"+id+"', '"
-                    +voornaam+"', '" +achternaam+"', '" +adres+"', '" +postcode+"', '" +woonplaats+"', " +
-                    "'" +geboortedatum+"', '" +telefoonnummer+"', '" +mailadres+"', '" +wachtwoord+"')");
+            statement.execute("INSERT INTO gebruiker(id, voornaam, achternaam, wachtwoord ) values ('"+id+"', '"
+                    +voornaam+"', '" +achternaam+"', '" +wachtwoord+"')");
             return Response.ok().build();
         } catch (Exception e){
             System.out.println(e.getMessage());
