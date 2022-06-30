4774b5644f6a8f406e354a5bbce12ff0d232ea20
Lobohuargo822 <jeroenbaltjes@hotmail.nl>
2022-06-30 12:03:56 +0200

    feat: changed databaseQuery

--- src/main/java/com/example/demo/data/databaseQuery.java
+++ src/main/java/com/example/demo/data/databaseQuery.java
@@ -18,9 +18,12 @@ public class databaseQuery {
         } catch (ClassNotFoundException e) {
             System.out.println("Class not found " + e);
         }
-        String jdbcURL = "jdbc:postgresql://localhost:5433/IpassHerkansing";
-        String username = "postgres";
-        String password = "T1mmyD3Kat";
+//        String jdbcURL = "jdbc:postgresql://localhost:5433/IpassHerkansing";
+//        String username = "postgres";
+//        String password = "T1mmyD3Kat";
+        String jdbcURL = "jdbc:postgres://cqqsytyunzsftj:7cdd55345624db304d413948081dadae66cab2c05caa6023c45c206c3127689c@ec2-63-34-180-86.eu-west-1.compute.amazonaws.com:5432/d3en7dhurf5e78";
+        String username = "cqqsytyunzsftj";
+        String password = "7cdd55345624db304d413948081dadae66cab2c05caa6023c45c206c3127689c";
         databaseQuery.connection = getConnection(jdbcURL, username, password);
     }
 
